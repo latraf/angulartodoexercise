77d025176fef08a5196ba5e19b5ae5519b83a28e
rflat <core.safecrlf>
2016-07-07 00:03:28 +0800

    Add Not Important To Do tab

--- assets/js/controllers/todo-ctrl.js
+++ assets/js/controllers/todo-ctrl.js
@@ -7,64 +7,52 @@
 
 		TodoCtrl.$inject = ['$scope', '$mdDialog'];
 
-		function TodoCtrl($scope, $mdDialog, $event) {
+		function TodoCtrl($scope, $mdDialog) {
 			var isImportant = false;
 			var title;
 			var notes;
 			var item;
-			
-			// $scope.todo.isImportant = isImportant;       
+			var isDone = false;
+		
 			$scope.todos = [
 				{ 	name: 'yani',
 					additional: 'hi',
-					isImportant: false 
+					isImportant: false,
+					isDone: false
 				},
 				{ 	name: 'oppa',
 					additional: 'hello po',
-					isImportant: true 
+					isImportant: true,
+					isDone: true
 				},
 				{ 	name: 'markjaykun',
 					additional: null,
-					isImportant: true 
+					isImportant: true,
+					isDone: false
 				}
-
 			];
 
+			$scope.doneTodos = [];
+			$scope.importantTodos = [];
+
 			$scope.submit = function() {
-					// $scope.checkForm();
-					title = $scope.todo.title;
-					notes = $scope.todo.notes;	
-					// $scope.checkForm(title);
-					// console.log(title);
-					// console.log(notes);
-					// console.log(isImportant);
-					if(title === null || title === undefined || title === ""){
-						// console.log("no entry");
-					}
-					else{
-						$scope.pushValues();
-						// console.log("PUSH");
-						$scope.reset();
-					}
-					// console.log($scope.todos.length);
-					$scope.closeDialog();
-				// }
+				title = $scope.todo.title;
+				notes = $scope.todo.notes;	
+				if(title === null || title === undefined || title === ""){ }
+				else{
+					$scope.pushValues();
+					$scope.reset();
+				}
+				$scope.closeDialog();
 			}
 
 			$scope.change = function() {
-				console.log("before: " + isImportant);
 				isImportant = !isImportant;
-				// $scope.todo.isImportant = isImportant;
-				console.log("after: " + isImportant);
-
 			}
 
 			$scope.changeStar = function(item) {
-				console.log("before: " + isImportant);
 				isImportant = !isImportant;
 				item.isImportant = isImportant;
-				console.log("after: " + isImportant);
-
 			}
 
 			$scope.reset = function() {
@@ -77,10 +65,10 @@
 				item = {
 					name: $scope.todo.title,
 					additional: $scope.todo.notes,
-					isImportant: isImportant
+					isImportant: isImportant,
+					isDone: isDone
 				};
 				$scope.todos.push(item);
-				// console.log($scope.todos.length);
 			}
 
 			$scope.addTodo = function() {
@@ -95,8 +83,6 @@
 
 			$scope.remove = function(item) {
 				var index = $scope.todos.indexOf(item);
-
-				// console.log(index);
 				$scope.todos.splice($scope.todos[index], 1);
 			}
 
@@ -116,34 +102,19 @@
 						$scope.editTitle = item.name;
 						$scope.editNotes = item.additional;
 						var newIsImportant = false;
-						// console.log("important: " + item.isImportant);
 						$scope.editImportant = item.isImportant;
-						// console.log("edit important: " + $scope.editImportant);
-
 						$scope.newChange = function() {
-							// console.log("before" + newIsImportant);
 							newIsImportant = !newIsImportant;
-							// console.log("after" + newIsImportant);
 						}
 
 						$scope.edit = function() {
 							var newTitle = $scope.edit.newTitle;
 							var newNotes = $scope.edit.newNotes;
-							// var newIsImportant = $scope.editImportant;
-							// console.log(newTitle);
-							// console.log(newNotes);
-							// console.log(newIsImportant);
 							if(newTitle === null || newTitle === undefined || newTitle === ""){
 								item.name = item.name;
-
 							}
 							else{
 								item.name = newTitle;
-								// console.log("new item name: " + item.name);
-								// item.additional = newNotes;
-								// console.log("new item name: " + item.additional);
-								// item.isImportant = newIsImportant;
-								// console.log("new item name: " + item.isImportant);
 							}
 							item.additional = newNotes;
 							item.isImportant = newIsImportant;
@@ -155,7 +126,6 @@
 						}
 					}
 				});
-				// console.log($scope.item.name);
 			}
 		}
 })();
